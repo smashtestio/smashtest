39c64e0b34adcab93b13e6fe37562d97291035f4
Peter Tesler <>
2019-04-01 23:03:33 -0400

    Added unit test for smashtest.js

---
+++ tests/core/smashtest.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const chaiSubset = require('chai-subset');
+const expect = chai.expect;
+const assert = chai.assert;
+const util = require('util');
+const utils = require('../../utils.js');
+const { spawnSync } = require('child_process');
+
+chai.use(chaiSubset);
+
+describe("smashtest.js", function() {
+    it.only("TEXT", function() {
+        const ls = spawnSync('ls', ['-lh']);
+        console.log(ls.stdout.toString());
+    });
+});
