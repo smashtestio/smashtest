b1b1ae7e2e6f2f8567b69abaea4cec40a5ad7207
Peter Tesler <>
2019-04-12 15:06:59 -0400

    Open any browser

--- packages/js/browser.js
+++ packages/js/browser.js
@@ -5,16 +5,17 @@ class Browser {
         this.driver = null;
     }
 
-    async open(browserName) {
+    async open(description) {
         this.driver = new webdriver
             .Builder()
-            .forBrowser(browserName)
+            .forBrowser('chrome')
             .build();
 
 
         // TODO: Start 'chrome 15 desktop headless 1900x1280' (any order)
         // TODO: headless browsers too
         // TODO: Log the version, dimensions, etc.
+        // TODO: description could be "any"
     }
 
     async get(url) {
