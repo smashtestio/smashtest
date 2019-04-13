3d7de8f7f3007a507a699f4a22290bfce2c412af
Peter Tesler <>
2019-04-13 01:42:20 -0400

    Updated unit test

--- tests/core/tree.js
+++ tests/core/tree.js
@@ -11725,7 +11725,7 @@ H -
             expect(tree.branches[0].steps).to.have.lengthOf(3);
             expect(tree.branches).to.containSubsetInOrder([
                 {
-                    steps: [ { text: "D" }, { text: "E" }, { text: "F", isDebug: true, isDebugBefore: true }  ]
+                    steps: [ { text: "D" }, { text: "E" }, { text: "F", isDebug: true, isDebugAfter: true }  ]
                 }
             ]);
         });
