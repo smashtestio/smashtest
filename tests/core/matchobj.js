3a1ffa1dd77833fe922a45954970302040001a0c
Peter Tesler <>
2019-04-17 22:36:54 -0400

    Work on matchobj.js

---
+++ tests/core/matchobj.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+const matchobj = require('./matchobj.js');
+
+describe("matchobj", () => {
+    describe("matchObjComponent()", () => {
+        it.skip("TEXT", () => {
+
+        });
+    });
+
+    describe("matchObj()", () => {
+        it.skip("TEXT", () => {
+
+        });
+    });
+
+    describe("matchJson()", () => {
+        it.skip("TEXT", () => {
+
+        });
+    });
+});
