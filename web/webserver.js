535ad8ef5b403b26e251a84b3984bb0c3829b697
Peter Tesler <>
2019-06-01 00:20:04 -0400

    Website

---
+++ web/webserver.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const fs = require('fs');
+const https = null;
+const http = require('http');
+
+const USE_PROD = false;
+const USE_SSL = false;
+
+let port = null;
+let domain = null;
+
+if(USE_PROD) {
+    port = USE_SSL ? 443 : 80;
+    domain = `${USE_SSL ? 'https' : 'http'}://smashtest.io`;
+}
+else { // local
+    port = USE_SSL ? 8081 : 8080;
+    domain = `${USE_SSL ? 'https' : 'http'}://localhost:${port}`;
+}
+
+let ca = null;
+let privateKey = null;
+let certificate = null;
+
+if(USE_SSL) {
+    https = require('https');
+
+    if(USE_PROD) {
+        ca = fs.readFileSync(__dirname + '/ssl/smashtest_io.ca-bundle', 'utf8');
+        privateKey = fs.readFileSync(__dirname + '/ssl/smashtest_io.key', 'utf8');
+        certificate = fs.readFileSync(__dirname + '/ssl/smashtest_io.crt', 'utf8');
+    }
+    else { // local
+        ca = null;
+        privateKey = fs.readFileSync(__dirname + '/ssl/local.key', 'utf8');
+        certificate = fs.readFileSync(__dirname + '/ssl/local.cert', 'utf8');
+    }
+}
+
+let app = express();
+
+// Homepage
+app.get(/^\/$/, (req, res) => {
+    let file = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    res.status(200).send(file);
+});
+
+// For serving up public-content/ directory
+app.use(express.static(__dirname + '/public-content'));
+
+// Generic error handler
+app.use((err, req, res, next) => {
+    res.status(404).send('404 Not Found');
+});
+
+// Start the server
+if(USE_SSL) {
+    var httpsServer = https.createServer({ ca: ca, key: privateKey, cert: certificate }, app);
+    httpsServer.listen(port, () => {
+        console.log(`HTTPS server running on port ${port}`);
+    });
+
+    // Server for redirecting http to https
+    var httpServer = http.createServer((req, res) => {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    });
+    httpServer.listen(port);
+}
+else {
+    var httpServer = http.createServer(app).listen(port, () => {
+        console.log(`HTTP server running on port ${port}`);
+    });
+}
