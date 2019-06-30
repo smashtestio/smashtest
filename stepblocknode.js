21271fc53a2fe31376cf1b3a90222e811459ae77
Peter Tesler <>
2019-06-29 23:40:02 -0400

    Fixed spacing in elementfinder.smash

--- stepblocknode.js
+++ stepblocknode.js
@@ -2,6 +2,7 @@ const StepNode = require('./stepnode.js');
 
 /**
  * Represents a plain step block (non-multi-level) within a Tree
+ * NOTE: Multi-level step blocks are implemented as function declaration/call pairs under the hood
  */
 class StepBlockNode extends StepNode {
     constructor() {
