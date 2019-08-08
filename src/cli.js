50474e559b5ec8864e0b2d17576004f5ff96c7ad
Peter Tesler <vptes1@gmail.com>
2019-08-08 12:57:24 -0400

    Version increment

--- src/cli.js
+++ src/cli.js
@@ -35,7 +35,7 @@ const PROGRESS_BAR_ON = true;
 let fullRun = false;
 
 console.log(hRule);
-console.log(yellowChalk.bold("Smashtest 1.0.24 BETA"));
+console.log(yellowChalk.bold("Smashtest 1.0.25 BETA"));
 console.log("");
 
 // ***************************************
